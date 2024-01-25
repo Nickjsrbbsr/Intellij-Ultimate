acbe42d8a3daa83eef105135a8d9a279c3bb1045
Nickjsrbbsr <nickjsrbbsr@gmail.com>
2024-01-25 05:32:05 +0530

    helo

--- Spring-boot1/src/main/java/org/example/mandal/springboot1/entity/Person.java
+++ Spring-boot1/src/main/java/org/example/mandal/springboot1/entity/Person.java
@@ -1,5 +1,8 @@
 package org.example.mandal.springboot1.entity;
-import jakarta.persistence.Entity;
+//import jakarta.persistence.Entity;
+import org.springframework.data.annotation.Id;
+
 @Entity
 public class Person {
+    @Id
 }
